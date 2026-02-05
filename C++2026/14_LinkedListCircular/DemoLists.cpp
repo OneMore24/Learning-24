52713aeb5fe438622b4fde1c8af80f185a8e742f
OneMore24 <alanjesus1946@outlook.com>
2026-02-05 17:10:12 -0500

    change LinkedListCircular

---
+++ C++2026/14_LinkedListCircular/DemoLists.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "containers/lists.h"
+
+using namespace std;
+
+using T1 = int;
+
+void DemoLists(){
+    CLinkedList< AscendingTrait<T1> > l1;
+
+    l1.Insert(20, 5);
+    l1.Insert(30, 3);
+    l1.Insert(50, 250000);
+    cout << l1 << endl;
+}
