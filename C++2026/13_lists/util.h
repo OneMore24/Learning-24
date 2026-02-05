7d49d2ba5a7911893407958a10e4453f1cbc2c9f
OneMore24 <alanjesus1946@outlook.com>
2026-02-05 11:39:42 -0500

    add files duplicated

---
+++ C++2026/13_lists/util.h
@@ -0,0 +1,14 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include "general/types.h"
+#include "foreach.h"
+
+template <typename T>
+void intercambiar(T &a, T &b){
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+#endif // __UTIL_H__
